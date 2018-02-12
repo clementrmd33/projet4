dec07ea69e84c6154246c2f8b1a0ae38a5e0c6c1
clement <cleraymond33@hotmail.fr>
2018-02-12 14:59:01 +0100

    creation des fichiers bootstrap et images, mise en place de l'include header

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Blog de Jean Forteroche</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="boot/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid" id="container_img">
+        <div class="row" id="container_header">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="navbar-brand">BLOG DE JEAN FORTEROCHE</div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="nav justify-content-end">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Episodes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Me contacter</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">S'identifier</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="container" id="description-header">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Bienvenue sur mon blog</p>
+                    <p>Pour lire mon nouveau roman</p>
+                    <button type="button" class="btn btn-primary">Cliquez ici</button>
+                </div>
+            </div>
+        </div>
+    </div>
