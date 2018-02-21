70e76149d0231a810138f4b9ab84578296fdbefa
clement <cleraymond33@hotmail.fr>
2018-02-21 23:00:58 +0100

    creation du fichier template.php

---
+++ view/template.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Blog de Jean Forteroche</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link href="contenu/css/style.css" rel="stylesheet">
+</head>
+<body>
+  <div id="container">
+    <header>
+      <h1>BLOG DE JEAN FORTEROCHE</h1>
+      <nav>
+        <ul>
+          <li><i class="fas fa-home"></i><a href="#">Accueil</a></li>
+          <li><i class="fas fa-book"></i><a href="#">Chapitres</a><i class="fas fa-sort-down"></i></li>
+          <li><i class="fas fa-pencil-alt"></i><a href="#">S'identifier</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div id="descriptif-container">
+      <p>Bienvenue sur mon blog</br>Pour lire mon nouveau roman</p>
+      <button>Cliquez ici</button>
+    </div>
+  </div>
+  
+    <?= $content ?>
+
+  <footer>
+    <p>Mention légale | Site réalisé par Clément RAYMOND pour la formation Openclassroom</p>
+  </footer>
+</body>
+</html>
